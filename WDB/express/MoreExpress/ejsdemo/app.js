234b867bc717ff3cb18ebc1f311a8f4a3ed4890e
onettrage <ness2k@gmail.com>
2018-09-26 13:51:31 +0100

    using partials and templates
    Learning how to use partials and templates; code has been made dryer now that partials are in use

--- WDB/express/MoreExpress/ejsdemo/app.js
+++ WDB/express/MoreExpress/ejsdemo/app.js
@@ -1,17 +1,21 @@
 var express = require("express");
 var app = express();
 
+// enables the loading of stylesheets from the "public" folder
+app.use(express.static("public"));
+// allows the rendering of ejs files without specifying .ejs each time
+app.set("view engine", "ejs");
+
 app.get("/", function(req, res){
-    res.render("home.ejs");
+    res.render("home");
 });
 
 app.get("/omg/:thing", function(req, res){
     var thing = req.params.thing;
-        res.render("omg.ejs", {thingVar: thing}
+        res.render("omg", {thingVar: thing}
        );
 });
 
-// updated app.js with posts
 app.get("/posts", function(req, res) {
     var posts = [
         {title: "Dude, where's my car", author: "Jamal"},
@@ -19,7 +23,7 @@ app.get("/posts", function(req, res) {
         {title: "It puts the lotion on the skin", author: "Max"}
         ];
         
-        res.render("posts.ejs", {posts: posts})
+        res.render("posts", {posts: posts})
 })
 
 app.listen(process.env.PORT, process.env.IP, function(){
