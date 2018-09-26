53888458c6901d4cec1ab4b28a6397e3a9069108
onettrage <ness2k@gmail.com>
2018-09-26 12:50:06 +0100

    Updating app.js
    Updated app,js to show posts/comments, and how to render content on a page using <%= %>, vs just running background code via <% %>

--- WDB/express/MoreExpress/ejsdemo/app.js
+++ WDB/express/MoreExpress/ejsdemo/app.js
@@ -9,6 +9,17 @@ app.get("/omg/:thing", function(req, res){
     var thing = req.params.thing;
         res.render("omg.ejs", {thingVar: thing}
        );
+});
+
+// updated app.js with posts
+app.get("/posts", function(req, res) {
+    var posts = [
+        {title: "Dude, where's my car", author: "Jamal"},
+        {title: "Has anyone seen my baby", author: "Betty"},
+        {title: "It puts the lotion on the skin", author: "Max"}
+        ];
+        
+        res.render("posts.ejs", {posts: posts})
 })
 
 app.listen(process.env.PORT, process.env.IP, function(){
